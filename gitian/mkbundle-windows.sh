57e43529d5edafff2b18af6a7766c05f72e1fb88
Mike Perry <mikeperry-git@torproject.org>
2013-05-09 22:06:36 -0700

    Switch Windows system to the True Gitian Way.

--- gitian/mkbundle-windows.sh
+++ gitian/mkbundle-windows.sh
@@ -8,25 +8,12 @@
 WRAPPER_DIR=$PWD
 GITIAN_DIR=$PWD/../../gitian-builder.git
 DESCRIPTOR_DIR=$PWD/descriptors/
-BUNDLE_DIR=$PWD/bundle-win32/
 
 if [ ! -f $GITIAN_DIR/bin/gbuild ];
 then
   echo "Gitian not found. You need a Gitian checkout in $GITIAN_DIR"
   exit 1
 fi
-
-# Set up the bundle dir skeleton
-if [ ! -d $BUNDLE_DIR ];
-then
-  mkdir -p $BUNDLE_DIR/32/FirefoxPortable/App/Firefox
-  mkdir -p $BUNDLE_DIR/32/FirefoxPortable/Data/profile/extensions
-  mkdir -p $BUNDLE_DIR/32/App
-  mkdir -p $BUNDLE_DIR/32/Data/Tor
-  mkdir -p $BUNDLE_DIR/32/Docs
-fi
-
-mkdir -p $WRAPPER_DIR/../Bundle-Data/windows/FirefoxPortable/Data/profile/extensions/
 
 cd $GITIAN_DIR
 export PATH=$PATH:$PWD/libexec
@@ -53,6 +40,21 @@ fi
 
 echo "pref(\"torbrowser.version\", \"$TORBROWSER_VERSION\");" > $GITIAN_DIR/inputs/torbrowser.version 
 
+# FIXME: Alpha vs non-alpha xpis??
+cd $GITIAN_DIR/inputs/
+ln -sf $NOSCRIPT_PACKAGE noscript@noscript.net.xpi
+ln -sf $TORBUTTON_PACKAGE torbutton@torproject.org.xpi
+ln -sf $HTTPSE_PACKAGE https-everywhere@eff.org.xpi
+ln -sf $PDFJS_PACKAGE uriloader@pdf.js.xpi
+
+cd $WRAPPER_DIR/..
+zip -rX $GITIAN_DIR/inputs/relativelink-src.zip ./RelativeLink/ 
+
+cd ./Bundle-Data/windows
+zip -rX $GITIAN_DIR/inputs/windows-skeleton.zip ./
+
+cd $GITIAN_DIR
+
 ./bin/gbuild --commit tor-launcher=$TORLAUNCHER_TAG,tor-browser=$TORBROWSER_TAG $DESCRIPTOR_DIR/windows/gitian-firefox.yml
 while [ $? -ne 0 ];
 do
@@ -60,9 +62,7 @@ do
   ./bin/gbuild --commit tor-launcher=$TORLAUNCHER_TAG,tor-browser=$TORBROWSER_TAG $DESCRIPTOR_DIR/windows/gitian-firefox.yml
 done
 
-cp -a build/out/bin/32/firefox/* $BUNDLE_DIR/32/FirefoxPortable/App/Firefox/
-
-cp -a build/out/bin/torlauncher*.xpi $WRAPPER_DIR/../Bundle-Data/windows/FirefoxPortable/Data/profile/extensions/torlauncher@torproject.org.xpi
+cp -a build/out/tor-browser-gbuilt.zip $GITIAN_DIR/inputs/
 
 ./bin/gbuild --commit tor=$TOR_TAG $DESCRIPTOR_DIR/windows/gitian-tor.yml
 while [ $? -ne 0 ];
@@ -71,33 +71,17 @@ do
   ./bin/gbuild --commit tor=$TOR_TAG $DESCRIPTOR_DIR/windows/gitian-tor.yml
 done
 
-cp -a build/out/bin/32/tor $BUNDLE_DIR/32/App/
-cp -a build/out/lib/32/lib* $BUNDLE_DIR/32/App/
+cp -a build/out/tor-gbuilt.zip $GITIAN_DIR/inputs/
 
-# FIXME: Alpha vs non-alpha xpis??
-# FIXME: NoScript versioning??
-cp -a $GITIAN_DIR/inputs/$NOSCRIPT_PACKAGE $WRAPPER_DIR/../Bundle-Data/windows/FirefoxPortable/Data/profile/extensions/noscript@noscript.net.xpi
-cp -a $GITIAN_DIR/inputs/$TORBUTTON_PACKAGE $WRAPPER_DIR/../Bundle-Data/windows/FirefoxPortable/Data/profile/extensions/torbutton@torproject.org.xpi
-cp -a $GITIAN_DIR/inputs/$HTTPSE_PACKAGE $WRAPPER_DIR/../Bundle-Data/windows/FirefoxPortable/Data/profile/extensions/https-everywhere@eff.org.xpi
-cp -a $GITIAN_DIR/inputs/$PDFJS_PACKAGE $WRAPPER_DIR/../Bundle-Data/windows/FirefoxPortable/Data/profile/extensions/uriloader@pdf.js.xpi
+./bin/gbuild --commit tbb-windows-installer=$NSIS_TAG $DESCRIPTOR_DIR/windows/gitian-bundle.yml
+while [ $? -ne 0 ];
+do
+  mv var/build.log ./bundle-fail-win32.log.`date +%Y%m%d%H%M%S`
+  ./bin/gbuild --commit tbb-windows-installer=$NSIS_TAG $DESCRIPTOR_DIR/windows/gitian-bundle.yml
+done
 
-cp -a $WRAPPER_DIR/../Bundle-Data/windows/* $BUNDLE_DIR/32/
-
-# XXX: Compile relativelink.exe
-#cp -a $WRAPPER_DIR/../RelativeLink/ $BUNDLE_DIR/32/start-tor-browser
-
-# FIXME: Fix this path juggling nonsense
-
-cd $BUNDLE_DIR
-# XXX: 7zip?? NSIS?? Should this be done in its own gitian descriptor?
-cp -a 32 tor-browser_en-US
-find tor-browser_en-US | xargs touch --date="2013-01-01 00:00:00"
-zip -rX tor-browser-$TORBROWSER_VERSION-en_US.zip tor-browser_en-US
-rm -rf tor-browser_en-US
-cd ..
-
+cp -a build/out/torbrowser-install.exe $WRAPPER_DIR
 
 # FIXME: localization
-
 # FIXME: docs
 
