e75ec8619a6ea0740b27a34d35858450bceaa535
Mike Perry <mikeperry-git@torproject.org>
2013-06-03 18:04:22 -0700

    Record this repo checkout in the bundle versions too.

--- gitian/record-inputs.sh
+++ gitian/record-inputs.sh
@@ -2,6 +2,8 @@
 
 . ./versions
 
+WRAPPER_DIR=$PWD
+
 if [ -n $1 ]; then
   INPUTS_DIR=../../gitian-builder/inputs
 else
@@ -20,3 +22,5 @@ sha256sum uriloader@pdf.js.xpi >> bundle.inputs
 echo "`cd torbutton && git log --format=%H -1` torbutton.git" >> bundle.inputs
 echo "`cd tor-launcher && git log --format=%H -1` tor-launcher.git" >> bundle.inputs
 echo "`cd https-everywhere && git log --format=%H -1` https-everywhere.git" >> bundle.inputs
+echo "`cd $WRAPPER_DIR && git log --format=%H -1` tor-browser-bundle.git" >> bundle.inputs
+
