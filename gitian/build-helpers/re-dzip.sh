ed2b583243848dc8241ee8a8d6fd4706ba57a61c
Mike Perry <mikeperry-git@torproject.org>
2013-06-18 13:29:51 -0700

    Fix permissions differences in omni.ja files.

--- gitian/build-helpers/re-dzip.sh
+++ gitian/build-helpers/re-dzip.sh
@@ -6,6 +6,8 @@ ZIPFILE=`basename $1`
 mkdir tmp_dzip
 cd tmp_dzip
 unzip ../$1
+find . -type f -exec chmod 644 {} \;
+find . -type d -exec chmod 755 {} \;
 find . | sort | zip $ZIPOPTS -X -@ $ZIPFILE
 mv $ZIPFILE ../$1
 cd ..
