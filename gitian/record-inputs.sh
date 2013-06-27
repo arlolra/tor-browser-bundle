c6cc84e6792ad94b37e55696162360aaf954efac
Mike Perry <mikeperry-git@torproject.org>
2013-06-27 16:35:29 -0700

    Specify explicit tags when recording inputs.
    Another determinism issue..

--- gitian/record-inputs.sh
+++ gitian/record-inputs.sh
@@ -24,12 +24,28 @@ sha256sum noscript@noscript.net.xpi >> bundle.inputs
 sha256sum uriloader@pdf.js.xpi >> bundle.inputs
 sha256sum openssl-*gz >> bundle.inputs
 echo >> bundle.inputs
-echo "`cd zlib && git log --format=%H -1` zlib.git" >> bundle.inputs
-echo "`cd tor && git log --format=%H -1` tor.git" >> bundle.inputs
-echo "`cd torbutton && git log --format=%H -1` torbutton.git" >> bundle.inputs
-echo "`cd tor-launcher && git log --format=%H -1` tor-launcher.git" >> bundle.inputs
-echo "`cd https-everywhere && git log --format=%H -1` https-everywhere.git" >> bundle.inputs
-echo "`cd tbb-windows-installer && git log --format=%H -1` tbb-windows-installer.git" >> bundle.inputs
-echo "`cd $WRAPPER_DIR && git log --format=%H -1` tor-browser-bundle.git" >> bundle.inputs
+
+if [ "z$VERIFY_TAGS" = "z1" ];
+then
+  # If we're verifying tags, be explicit to gitian that we
+  # want to build from tags.
+  # XXX: Some things still have no tags
+  # NSIS_TAG=refs/tags/$NSIS_TAG
+  TORLAUNCHER_TAG=refs/tags/$TORLAUNCHER_TAG
+  TORBROWSER_TAG=refs/tags/$TORBROWSER_TAG
+  TORBUTTON_TAG=refs/tags/$TORBUTTON_TAG
+  TOR_TAG=refs/tags/$TOR_TAG
+  HTTPSE_TAG=refs/tags/$HTTPSE_TAG
+  ZLIB_TAG=refs/tags/$ZLIB_TAG
+  LIBEVENT_TAG=refs/tags/$LIBEVENT_TAG
+fi
+
+echo "`cd zlib && git log --format=%H -1 $ZLIB_TAG` zlib.git" >> bundle.inputs
+echo "`cd tor && git log --format=%H -1 $TOR_TAG` tor.git" >> bundle.inputs
+echo "`cd torbutton && git log --format=%H -1 $TORBUTTON_TAG` torbutton.git" >> bundle.inputs
+echo "`cd tor-launcher && git log --format=%H -1 $TORLAUNCHER_TAG` tor-launcher.git" >> bundle.inputs
+echo "`cd https-everywhere && git log --format=%H -1 $HTTPSE_TAG` https-everywhere.git" >> bundle.inputs
+echo "`cd tbb-windows-installer && git log --format=%H -1 $NSIS_TAG` tbb-windows-installer.git" >> bundle.inputs
 echo "`cd $INPUTS_DIR && git log --format=%H -1` gitian-builder.git" >> bundle.inputs
+echo "`cd $WRAPPER_DIR && git log --format=%H -1` tor-browser-bundle.git" >> bundle.inputs
 
