3279db32f147479af225d7106949e8dddd360dbb
Mike Perry <mikeperry-git@torproject.org>
2014-02-28 10:02:10 -0800

    Relocate bridges from torrc appendices to a prefs file.

---
+++ Bundle-Data/beta/bridge_prefs.js
@@ -0,0 +1,15 @@
+# Tor Launcher preferences (default bridges):
+pref("extensions.torlauncher.default_bridge_recommended_type", "obfs3");
+
+// Default bridges.
+pref("extensions.torlauncher.default_bridge.obfs3.1", "obfs3 83.212.101.3:60475 A09D536DD1752D542E1FBB3C9CE4449D51298239");
+pref("extensions.torlauncher.default_bridge.obfs3.2", "obfs3 169.229.59.74:31493 AF9F66B7B04F8FF6F32D455F05135250A16543C9");
+pref("extensions.torlauncher.default_bridge.obfs3.3", "obfs3 169.229.59.75:46328 AF9F66B7B04F8FF6F32D455F05135250A16543C9");
+pref("extensions.torlauncher.default_bridge.obfs3.4", "obfs3 209.141.36.236:45496 58D91C3A631F910F32E18A55441D5A0463BA66E2");
+pref("extensions.torlauncher.default_bridge.obfs3.5", "obfs3 208.79.90.242:35658 BA61757846841D64A83EA2514C766CB92F1FB41F");
+pref("extensions.torlauncher.default_bridge.obfs3.6", "obfs3 109.105.109.163:38980 9D7259A696F7DAB073043B28114112A46D36CFFD");
+pref("extensions.torlauncher.default_bridge.obfs3.7", "obfs3 109.105.109.163:47779 844B1F53FFD548C998F8D3B01B7E19FA07C3396E");
+
+pref("extensions.torlauncher.default_bridge.flashproxy.1", "flashproxy 0.0.1.0:1");
+
+pref("extensions.torlauncher.default_bridge.fte.1", "fte bridges.fteproxy.org:8080");
