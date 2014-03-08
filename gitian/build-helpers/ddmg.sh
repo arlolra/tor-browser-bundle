fb9453c51306ad39d261617e2cb3ba8e18368574
Mike Perry <mikeperry-git@torproject.org>
2014-03-08 13:46:23 -0800

    Bug 4261: Fix DMG permissions to work from /Applications
    Only seems to matter on some Mac versions... Very odd.

--- gitian/build-helpers/ddmg.sh
+++ gitian/build-helpers/ddmg.sh
@@ -5,14 +5,17 @@ export LC_ALL=C
 DMGFILE=$1
 shift
 
-find $@ -executable -exec chmod 700 {} \;
-find $@ ! -executable -exec chmod 600 {} \;
+# We need group readability for some Macs to be able to handle /Applications
+# installation. Still unclear exactly why this is -- it is not dependent on
+# OSX version...
+find $@ -executable -exec chmod 750 {} \;
+find $@ ! -executable -exec chmod 640 {} \;
 
 cd $@
 find . -type f | sed -e 's/^\.\///' | sort | xargs -i echo "{}={}" > ~/build/filelist.txt
 find . -type l | sed -e 's/^\.\///' | sort | xargs -i echo "{}={}" >> ~/build/filelist.txt
 
-mkisofs -D -V "Tor Browser" -no-pad -R -apple -o ~/build/tbb-uncompressed.dmg -path-list ~/build/filelist.txt -graft-points -dir-mode 0700 -new-dir-mode 0700
+mkisofs -D -V "Tor Browser" -no-pad -R -apple -o ~/build/tbb-uncompressed.dmg -path-list ~/build/filelist.txt -graft-points -gid 20 -dir-mode 0750 -new-dir-mode 0750
 
 cd ~/build
 
