681b1598bbbb0704f620bc0e27624a46d6f51f89
Georg Koppen <gk@torproject.org>
2014-04-02 10:30:11 +0000

    Bug 11240: Don't run chmod on (dangling) symlinks.

--- gitian/build-helpers/dtar.sh
+++ gitian/build-helpers/dtar.sh
@@ -5,7 +5,8 @@ export LC_ALL=C
 TARFILE=$1
 shift
 
-find $@ -executable -exec chmod 700 {} \;
-find $@ ! -executable -exec chmod 600 {} \;
+# No need to execute chmod on (possibly) dangling symlinks.
+find $@ ! -type l -executable -exec chmod 700 {} \;
+find $@ ! -type l ! -executable -exec chmod 600 {} \;
 
 find $@ | sort | tar --no-recursion -Jcvf $TARFILE -T -
