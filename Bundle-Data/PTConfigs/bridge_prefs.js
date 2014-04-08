ae9d19adf7935782d926919c84977f703b7ebadf
Kevin P. Dyer <kpdyer@gmail.com>
2014-04-08 12:24:27 -0700

    Update default PT Bridges.

--- Bundle-Data/PTConfigs/bridge_prefs.js
+++ Bundle-Data/PTConfigs/bridge_prefs.js
@@ -2,13 +2,13 @@
 pref("extensions.torlauncher.default_bridge_recommended_type", "obfs3");
 
 // Default bridges.
-pref("extensions.torlauncher.default_bridge.obfs3.1", "obfs3 83.212.101.3:60475 A09D536DD1752D542E1FBB3C9CE4449D51298239");
+pref("extensions.torlauncher.default_bridge.obfs3.1", "obfs3 83.212.101.3:80 A09D536DD1752D542E1FBB3C9CE4449D51298239");
 pref("extensions.torlauncher.default_bridge.obfs3.2", "obfs3 169.229.59.74:31493 AF9F66B7B04F8FF6F32D455F05135250A16543C9");
 pref("extensions.torlauncher.default_bridge.obfs3.3", "obfs3 169.229.59.75:46328 AF9F66B7B04F8FF6F32D455F05135250A16543C9");
 pref("extensions.torlauncher.default_bridge.obfs3.4", "obfs3 209.141.36.236:45496 58D91C3A631F910F32E18A55441D5A0463BA66E2");
 pref("extensions.torlauncher.default_bridge.obfs3.5", "obfs3 208.79.90.242:35658 BA61757846841D64A83EA2514C766CB92F1FB41F");
-pref("extensions.torlauncher.default_bridge.obfs3.6", "obfs3 109.105.109.163:38980 9D7259A696F7DAB073043B28114112A46D36CFFD");
-pref("extensions.torlauncher.default_bridge.obfs3.7", "obfs3 109.105.109.163:47779 844B1F53FFD548C998F8D3B01B7E19FA07C3396E");
+pref("extensions.torlauncher.default_bridge.obfs3.6", "obfs3 109.105.109.163:38980 1E05F577A0EC0213F971D81BF4D86A9E4E8229ED");
+pref("extensions.torlauncher.default_bridge.obfs3.7", "obfs3 109.105.109.163:47779 4C331FA9B3D1D6D8FB0D8FBBF0C259C360D97E6A");
 
 pref("extensions.torlauncher.default_bridge.flashproxy.1", "flashproxy 0.0.1.0:1");
 pref("extensions.torlauncher.default_bridge.flashproxy.2", "flashproxy 0.0.1.0:2");
@@ -18,3 +18,6 @@ pref("extensions.torlauncher.default_bridge.flashproxy.5", "flashproxy 0.0.1.0:5
 
 pref("extensions.torlauncher.default_bridge.fte.1", "fte 131.252.210.150:8080 271EC1874E40FE65C145C6397AA34FFF7008E50E");
 pref("extensions.torlauncher.default_bridge.fte.2", "fte 79.125.3.12:8080 17AF9F9F4E57614A060B7221DCCEDB8BB546DD73");
+pref("extensions.torlauncher.default_bridge.fte.3", "fte 128.105.214.161:8080 1E326AAFB3FCB515015250D8FCCC8E37F91A153B");
+pref("extensions.torlauncher.default_bridge.fte.4", "fte 128.105.214.162:8080 FC562097E1951DCC41B7D7F324D88157119BB56D");
+pref("extensions.torlauncher.default_bridge.fte.5", "fte 128.105.214.163:8080 A17A40775FBD2CA1184BF80BFC330A77ECF9D0E9");
