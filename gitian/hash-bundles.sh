59aad368f24170654781fd4cd538337b0ec2312c
Mike Perry <mikeperry-git@torproject.org>
2015-01-27 08:46:08 -0800

    Bug 14422: Make incrementals by default.
    Also create a "resume" rule to make it easier to resume a build, including
    remaking incrementals, and update the documentation.
    
    Also remove the -beta rules, as we don't use those.

--- gitian/hash-bundles.sh
+++ gitian/hash-bundles.sh
@@ -24,11 +24,22 @@ sha256sum `ls -1 | grep -v '\.incremental\.mar$' | sort` > sha256sums.txt
 if ls -1 | grep -q '\.incremental\.mar$'
 then
     sha256sum `ls -1 | grep '\.incremental\.mar$' | sort` > sha256sums.incrementals.txt
+    echo
+    echo "If this is an official build, you should now sign your result with: "
+    echo "  make sign"
+    echo
+    echo "In either case, you can check against any official builds with: "
+    echo "  make match"
+else
+    echo
+    echo "It appears that this build did not generate any incremental update (.mar) files"
+    echo
+    echo "If your goal is to reproduce the entire release, you still need to download"
+    echo "the mar files from the previous release from the appropriate directory in: "
+    echo "  https://archive.torproject.org/tor-package-archive/torbrowser/"
+    echo
+    echo "After that, you will need to make the incremental updates with: "
+    echo "  make incrementals && make hash"
 fi
 
-echo
-echo "If this is an official build, you should now sign your result with: "
-echo "  make sign"
-echo
-echo "In either case, you can check against any official builds with: "
-echo "  make match"
+
