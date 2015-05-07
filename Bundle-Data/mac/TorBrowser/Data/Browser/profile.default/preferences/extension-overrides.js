a9619e0a54d7c134325ad5a9496ffc67d07b0dcf
Mike Perry <mikeperry-git@torproject.org>
2015-05-06 18:56:45 -0700

    Bug 15945: Disable NoScript's ClearClick protection for now.
    Bug #14985 has analysis toward a more complete fix for enabling it.

--- Bundle-Data/mac/TorBrowser/Data/Browser/profile.default/preferences/extension-overrides.js
+++ Bundle-Data/mac/TorBrowser/Data/Browser/profile.default/preferences/extension-overrides.js
@@ -61,3 +61,4 @@ pref("noscript.cascadePermissions", true);
 pref("noscript.restrictSubdocScripting", true);
 pref("noscript.showVolatilePrivatePermissionsToggle", false);
 pref("noscript.volatilePrivatePermissions", true);
+pref("noscript.clearClick", 0);
