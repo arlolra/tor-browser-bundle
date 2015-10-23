258d96190368238b7e6419fa768ffddef2931ac7
Linus Nordberg <linus@torproject.org>
2015-10-23 09:03:12 +0200

    Bug 17415: Indicate whether remote was updated or not using exit codes.

--- tools/continuous-builds/checkout-tbb.sh
+++ tools/continuous-builds/checkout-tbb.sh
@@ -1,8 +1,19 @@
 #! /bin/sh
 
-BUILDDIR=$1
-[ -z "$BUILDDIR" ] && BUILDDIR=~/usr/src/tor-browser-bundle/gitian
+# Checkout branch $2 (default=master) in directory $1 and update it.
+# Exit 0 if HEAD changed due to the update.
+# Exit 1 if HEAD has not changed.
+# Proposed use:
+# $ checkout-tbb.sh && build-tbb.sh
 
-cd $BUILDDIR || exit 1
-git checkout master
-git pull
+BUILDDIR=$HOME/usr/src/tor-browser-bundle/gitian
+[ -n "$1" ] && BUILDDIR="$1"
+BRANCH=master
+[ -n "$2" ] && BRANCH=$2
+
+cd "$BUILDDIR" || exit 2
+
+HEAD=$(git rev-parse $BRANCH)
+git checkout -q $BRANCH
+git pull -q
+[ $HEAD != $(git rev-parse $BRANCH) ]
