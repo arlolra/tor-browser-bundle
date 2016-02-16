9e1ff5bf59a07f9a4277d75e2331c47f297a6921
Nicolas Vigier <boklm@torproject.org>
2016-02-16 19:30:02 +0100

    Bug 18127: use $GITIAN_TAG in fetch-inputs.sh

--- gitian/fetch-inputs.sh
+++ gitian/fetch-inputs.sh
@@ -243,8 +243,7 @@ cd ..
 git remote set-url origin https://git.torproject.org/builders/gitian-builder.git
 git fetch origin
 git fetch --tags origin # XXX - why do we fetch tags specifically?
-git checkout tor-browser-builder-3
-git merge origin/tor-browser-builder-3
+git checkout "$GITIAN_TAG"
 cd inputs
 
 while read dir url tag; do
