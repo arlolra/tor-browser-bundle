55ba062a0ecda854d55f47f1e660746ad099581d
Georg Koppen <gk@torproject.org>
2016-12-08 12:48:18 +0000

    Revert "Bug 20147: (re-)dzip.sh: various improvements"
    This reverts commit a38d827c12ce75d13e18f19fc8f5bac8aca28c55.
    
    Surprisingly this breaks our builds. See the reopened #20417 for more
    details.

--- gitian/build-helpers/dzip.sh
+++ gitian/build-helpers/dzip.sh
@@ -1,13 +1,14 @@
-#!/bin/sh -e
+#!/bin/sh
 # Crappy deterministic zip wrapper
 export LC_ALL=C
 
-ZIPFILE=${1:?}
+ZIPFILE=$1
 shift
 
-if [ -n "$REFERENCE_DATETIME" ]; then
-	find "$@" -exec touch --date="$REFERENCE_DATETIME" -- {} +
-fi
-find "$@"   -executable -exec chmod 700 {} +
-find "$@" ! -executable -exec chmod 600 {} +
-find "$@" | sort | zip $ZIPOPTS -X -@ "$ZIPFILE"
+[ -n "$REFERENCE_DATETIME" ] && \
+	find $@ -exec touch --date="$REFERENCE_DATETIME" {} \;
+
+find $@ -executable -exec chmod 700 {} \;
+find $@ ! -executable -exec chmod 600 {} \;
+
+find $@ | sort | zip $ZIPOPTS -X -@ "$ZIPFILE"
--- gitian/build-helpers/re-dzip.sh
+++ gitian/build-helpers/re-dzip.sh
@@ -1,14 +1,17 @@
-#!/bin/sh -e
+#!/bin/sh
 # Crappy deterministic zip repackager
 export LC_ALL=C
 
-ZIPFILE_BASENAME=$(basename -- "${1:?}")
-TEMPDIR=tmp-re-dzip-$$
-RE_DZIP=$(readlink -f -- "$(which -- "$0")")
-PATH=$PATH:$(dirname "$RE_DZIP")
+ZIPFILE=`basename $1`
 
-mkdir "$TEMPDIR"
-unzip $UNZIPOPTS -d "$TEMPDIR" -- "$1"
-(cd "$TEMPDIR"; dzip.sh ./"$ZIPFILE_BASENAME" .)
-mv -- "$TEMPDIR"/"$ZIPFILE_BASENAME" "$1"
-rm -rf "$TEMPDIR"
+mkdir tmp_dzip
+cd tmp_dzip
+unzip ../$1
+[ -n "$REFERENCE_DATETIME" ] && \
+	find . -exec touch --date="$REFERENCE_DATETIME" {} \;
+find . -executable -exec chmod 700 {} \;
+find . ! -executable -exec chmod 600 {} \;
+find . | sort | zip $ZIPOPTS -X -@ $ZIPFILE
+mv $ZIPFILE ../$1
+cd ..
+rm -rf tmp_dzip
