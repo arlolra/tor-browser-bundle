732e8df18b1d883b48573b84c9e121ad72772159
Georg Koppen <gk@torproject.org>
2016-12-11 15:48:54 +0000

    Just take the version number for sandbox tag

--- gitian/mkbundle-linux.sh
+++ gitian/mkbundle-linux.sh
@@ -232,8 +232,15 @@ else
   echo
 fi
 
-if [ ! -f inputs/sandbox-$SANDBOX_TAG-linux32.zip -o \
-     ! -f inputs/sandbox-$SANDBOX_TAG-linux64.zip ];
+if [ "$SANDBOX_TAG" = "master" ]
+then
+  SB_TAG="master"
+else
+  SB_TAG=${SANDBOX_TAG#sandboxed-tor-browser-}
+fi
+
+if [ ! -f inputs/sandbox-$SB_TAG-linux32.zip -o \
+     ! -f inputs/sandbox-$SB_TAG-linux64.zip ];
 then
   echo
   echo "****** Starting Sandbox Component of Linux Bundle (5/6 for Linux) ******"
@@ -245,7 +252,7 @@ then
     exit 1
   fi
 
-  cp -a build/out/sandbox-$SANDBOX_TAG-linux*.zip inputs/
+  cp -a build/out/sandbox-$SB_TAG-linux*.zip inputs/
   #cp -a result/sandbox-linux-res.yml inputs/
 else
   echo
@@ -273,7 +280,7 @@ then
   cp -a build/out/*.mar $WRAPPER_DIR/$TORBROWSER_BUILDDIR/ || exit 1
   cp -a inputs/mar-tools-linux*.zip $WRAPPER_DIR/$TORBROWSER_BUILDDIR/ || exit 1
   cp -a inputs/*debug.zip $WRAPPER_DIR/$TORBROWSER_BUILDDIR/ || exit 1
-  cp -a inputs/sandbox-$SANDBOX_TAG-linux*.zip $WRAPPER_DIR/$TORBROWSER_BUILDDIR/ || exit 1
+  cp -a inputs/sandbox-$SB_TAG-linux*.zip $WRAPPER_DIR/$TORBROWSER_BUILDDIR/ || exit 1
   touch inputs/bundle-linux.gbuilt
 else
   echo
